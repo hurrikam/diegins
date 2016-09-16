ccb732b20a2dd3324460e40b1cdbe10ed2f7ab18
diego <hurrikam@hotmail.it>
2016-09-16 23:18:04 +0100

    Added missing case in mime types unit test

--- server/mimeTypesSpec.ts
+++ server/mimeTypesSpec.ts
@@ -5,6 +5,7 @@ describe('MimeTypes', () => {
 
     it('All MIME types are properly defined', () => {
         expect(MimeTypes.APPLICATION_JAVASCRIPT).to.equal('application/javascript');
+        expect(MimeTypes.APPLICATION_JSON).to.equal('application/json');
         expect(MimeTypes.IMAGE_PNG).to.equal('image/png');
         expect(MimeTypes.TEXT_CSS).to.equal('text/css');
         expect(MimeTypes.TEXT_HTML).to.equal('text/html');
