7747e6ac2d18bcc803a92bf696b375349f85f0a0
hurrikam <hurrikam@hotmail.it>
2019-08-22 21:35:26 +0100

    Fallback missing routes to index.html

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,20 +1,21 @@
 ﻿'use strict';
 
 import * as express from 'express';
+import { join } from 'path';
 import { urlencoded } from 'express';
 import { createServer } from 'http';
 import { initializeServices } from './services';
 import CommandManager from './commandManager';
 import { createCommands } from './commandFactory';
-
-const DIST_ROOT = 'dist';
+import { DIST_FOLDER } from './jobs/jobFileConstants';
 
 async function start() {
     await initializeServices();
     const port = 1337;
     const expressApp = express();
     expressApp.use(urlencoded({ extended: true }));
-    expressApp.use(express.static(DIST_ROOT));
+    expressApp.use(express.static(DIST_FOLDER));
+    expressApp.get('*', (req, res) => res.sendFile(join(DIST_FOLDER, 'index.html')));
     const server = createServer(expressApp);
     const commandManager = new CommandManager(expressApp);
     const commands = createCommands();
--- src/server/jobs/jobFileConstants.ts
+++ src/server/jobs/jobFileConstants.ts
@@ -2,6 +2,7 @@
 
 import { resolve, join } from 'path';
 
+export const DIST_FOLDER = resolve(join(__dirname, '../../../dist'));
 export const JOB_CONFIGURATIONS_FOLDER = resolve(join(__dirname, '../../../job_configurations'));
 export const JOB_CONFIG_FILE_NAME = 'config.json';
 export const JOB_LOG_FILE_NAME = 'log.txt';
